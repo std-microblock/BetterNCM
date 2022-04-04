cf26a0c14b5af87d9733aabd73430394f7d59c5e
MicroCBer <66859419+MicroCBer@users.noreply.github.com>
2022-04-05 00:04:30 +0800

    Update pluginmanager.js

--- addons/pluginmanager.js
+++ addons/pluginmanager.js
@@ -9,7 +9,7 @@ setInterval(function(){
         settings.style.zIndex="100000"
         settings.id="betterncmPM"
         // settings.style.background="yellow"
-        settings.innerHTML="<h2 class='s-fc1 f-ff2'>BetterNCM</h2><button onclick='fetch(\"https://music.163.com/betterncm_api/opensettings\").then(function(){document.location.reload()})'>编辑设置</button><button onclick='fetch(\"https://music.163.com/betterncm_api/opencsssettings\").then(function(){document.location.reload()})'>编辑CSS设置</button><button onclick='fetch(\"https://music.163.com/betterncm_api/openconfigfolder\")'>打开配置文件夹</button>"
+        settings.innerHTML="<h2 class='s-fc1 f-ff2'>BetterNCM</h2><button onclick='fetch(\"https://music.163.com/betterncm_api/opensettings\").then(function(){document.location.reload()})'>编辑设置</button><button onclick='fetch(\"https://music.163.com/betterncm_api/opencsssettings\").then(function(){document.location.reload()})'>编辑CSS设置</button><button onclick='fetch(\"https://music.163.com/betterncm_api/openconfigfolder\")'>打开配置文件夹</button><button onclick='document.location.reload()'>重载页面</button>"
         document.querySelector(".g-mn-set").appendChild(settings)
     }
 },100)
