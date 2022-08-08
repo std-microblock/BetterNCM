720ebd4d241441fe7a3dcb251d41157c3141b7fc
MicroCBer <66859419+MicroCBer@users.noreply.github.com>
2022-08-09 00:24:26 +0800

    改名 整理代码

--- runNCM.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cls
-echo Pending...
-taskkill /f /im cloudmusic.exe>nul
-
-ping localhost -n 2
-
-del "C:\Program Files (x86)\Netease\CloudMusic\msimg32.dll"
-copy I:\EasyCEFInject\Release\EasyCefInject.dll "C:\Program Files (x86)\Netease\CloudMusic\msimg32.dll">nul
