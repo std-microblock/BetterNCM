3503c752ff9b26882711049852cf8035075d4aac
mo-jinran <mojinran@outlook.com>
2022-11-14 23:08:28 +0800

    fix: DevTools

--- src/App.cpp
+++ src/App.cpp
@@ -650,12 +650,18 @@ App::App() {
 	EasyCEFHooks::onKeyEvent = [](auto client, auto browser, auto event) {
 		if (event->type == KEYEVENT_KEYUP && event->windows_key_code == 123) {
 			auto cef_browser_host = browser->get_host(browser);
-			CefWindowInfo windowInfo{};
-			CefBrowserSettings settings{};
-			CefPoint point{};
-			windowInfo.SetAsPopup(NULL, "EasyCEFInject DevTools");
-
-			cef_browser_host->show_dev_tools(cef_browser_host, &windowInfo, client, &settings, &point);
+			if (browser->get_host(browser)->has_dev_tools(cef_browser_host))
+			{
+				browser->get_host(browser)->close_dev_tools(cef_browser_host);
+			}
+			else
+			{
+				CefWindowInfo windowInfo {};
+				CefBrowserSettings settings {};
+				CefPoint point {};
+				windowInfo.SetAsPopup(NULL, "EasyCEFInject DevTools");
+				cef_browser_host->show_dev_tools(cef_browser_host, &windowInfo, client, &settings, &point);
+			}
 		}
 	};
 
