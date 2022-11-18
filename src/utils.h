460368e948e0749d586dd4337fa14013c817fad6
MicroCBer <66859419+MicroCBer@users.noreply.github.com>
2022-11-18 22:08:49 +0800

    chore: 0.2.5-pre

--- src/utils.h
+++ src/utils.h
@@ -10,7 +10,7 @@ string ws2s(const wstring& str);
 std::wstring s2ws(const std::string& s, bool isUtf8 = true);
 void write_file_text(const string& path, const string& text, bool append = false);
 string getEnvironment(const string& key);
-string getUCMPath();
+string getNCMPath();
 
 string get_command_line();
 
