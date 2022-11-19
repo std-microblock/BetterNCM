6979b999075f336f3d393b1287b491cd4568f6e3
MicroCBer <66859419+MicroCBer@users.noreply.github.com>
2022-11-19 23:41:53 +0800

    refactor: now parallelly rendering config and info part
    significantly improved loading speed of settings page

--- src/App.cpp
+++ src/App.cpp
@@ -6,7 +6,7 @@
 
 namespace fs = std::filesystem;
 
-const auto version = "0.2.4";
+const auto version = "0.2.5";
 
 extern string datapath;
 
@@ -249,18 +249,33 @@ function dom(tag, settings, ...children) {
 
 
 
-betterncm.utils.waitForElement(".g-mn-set").then(async (settingsDom) => {
-    let updatey;
+betterncm.utils.waitForElement(".g-mn-set").then((settingsDom)=>{(async () => {
+ settingsDom.prepend(
+        dom("div", { style: { marginLeft: "30px" } },
+            dom("div", { style: { display: "flex", flexDirection: "column", alignItems: "center" } },
+                dom("img", { src: "https://s1.ax1x.com/2022/08/11/vGlJN8.png", style: { width: "60px" } }),
+                dom("div", { innerText: "BetterNCM II", style: { fontSize: "20px", fontWeight: "700" } }),
+                dom("div", { innerText: "v" + await betterncm.app.getBetterNCMVersion() }),
+                dom("div",{class:["BetterNCM-PM-Updatey"]}),
+                dom("div", { style: { marginBottom: "20px" } },
+                    dom("a", { class: ["u-ibtn5", "u-ibtnsz8"], innerText: "Open Folder", onclick: async () => { await betterncm.app.exec(`explorer "${(await betterncm.app.getDataPath()).replace(/\//g,"\\")}"`,false,true) }, style: { margin: "5px" } }),
+					dom("a", { class: ["u-ibtn5", "u-ibtnsz8"], innerText: "Show Console", onclick: async () => { await betterncm.app.showConsole() }, style: { margin: "5px" } }),
+					dom("a", { class: ["u-ibtn5", "u-ibtnsz8"], innerText: "Reload Plugins", onclick: async () => { await betterncm.app.reloadPlugins();document.location.reload(); }, style: { margin: "5px" } }),
+                )
+            ),
+            dom("div", { class: ["BetterNCM-Plugin-Configs"] })
+        ))
+
     try {
         let currentVersion = await betterncm.app.getBetterNCMVersion()
         let ncmVersion = document.querySelector(".fstbtn>span").innerText.split(" ")[0]
         let online = await (await fetch("https://gitee.com/microblock/better-ncm-v2-data/raw/master/betterncm/betterncm.json")).json()
         let onlineSuitableVersions = online.versions.filter(v => v.supports.includes(ncmVersion))
 
-        if (onlineSuitableVersions.length === 0) updatey = dom("div", { innerText: decodeURI("BetterNCM%E6%9A%82%E6%9C%AA%E5%AE%98%E6%96%B9%E6%94%AF%E6%8C%81%E8%AF%A5%E7%89%88%E6%9C%AC%EF%BC%8C%E5%8F%AF%E8%83%BD%E4%BC%9A%E5%87%BA%E7%8E%B0Bug") });
+        if (onlineSuitableVersions.length === 0) document.querySelector(".BetterNCM-PM-Updatey").appendChild(dom("div", { innerText: decodeURI("BetterNCM%E6%9A%82%E6%9C%AA%E5%AE%98%E6%96%B9%E6%94%AF%E6%8C%81%E8%AF%A5%E7%89%88%E6%9C%AC%EF%BC%8C%E5%8F%AF%E8%83%BD%E4%BC%9A%E5%87%BA%E7%8E%B0Bug") }));
 
         if (currentVersion != onlineSuitableVersions[0].version) {
-            updatey = dom("div",
+            document.querySelector(".BetterNCM-PM-Updatey").appendChild(dom("div",
                 {
                     style:
                         { display: "flex", flexDirection: "column", alignItems: "center" }
@@ -278,27 +293,14 @@ betterncm.utils.waitForElement(".g-mn-set").then(async (settingsDom) => {
                                 betterncm.app.exec(`cmd /c @echo off & echo BetterNCM Updating... & cd "${ncmpath}" & taskkill /f /im cloudmusic.exe>nul & taskkill /f /im cloudmusicn.exe>nul & ping 127.0.0.1>nul & del msimg32.dll & move "${dllpath}" .\\msimg32.dll & start cloudmusic.exe`, true)
                             }
                         }
-                    }))
+                    })))
         }
 
     } catch (e) { }
 
-    settingsDom.prepend(
-        dom("div", { style: { marginLeft: "30px" } },
-            dom("div", { style: { display: "flex", flexDirection: "column", alignItems: "center" } },
-                dom("img", { src: "https://s1.ax1x.com/2022/08/11/vGlJN8.png", style: { width: "60px" } }),
-                dom("div", { innerText: "BetterNCM II", style: { fontSize: "20px", fontWeight: "700" } }),
-                dom("div", { innerText: "v" + await betterncm.app.getBetterNCMVersion() }),
-                updatey,
-                dom("div", { style: { marginBottom: "20px" } },
-                    dom("a", { class: ["u-ibtn5", "u-ibtnsz8"], innerText: "Open Folder", onclick: async () => { await betterncm.app.exec(`explorer "${(await betterncm.app.getDataPath()).replace(/\//g,"\\")}"`,false,true) }, style: { margin: "5px" } }),
-					dom("a", { class: ["u-ibtn5", "u-ibtnsz8"], innerText: "Show Console", onclick: async () => { await betterncm.app.showConsole() }, style: { margin: "5px" } }),
-					dom("a", { class: ["u-ibtn5", "u-ibtnsz8"], innerText: "Reload Plugins", onclick: async () => { await betterncm.app.reloadPlugins();document.location.reload(); }, style: { margin: "5px" } }),
-                )
-            ),
-            dom("div", { class: ["BetterNCM-Plugin-Configs"] })
-        ))
-
+   
+})();
+(async ()=>{
     await betterncm.utils.waitForFunction(() => window.loadedPlugins)
 
     const tools = {
@@ -317,6 +319,8 @@ betterncm.utils.waitForElement(".g-mn-set").then(async (settingsDom) => {
         document.querySelector(".BetterNCM-Plugin-Configs").appendChild(dom("div", { innerText: name, style: { fontSize: "18px", fontWeight: 600 } }))
         document.querySelector(".BetterNCM-Plugin-Configs").appendChild(dom("div", { style: { padding: "7px" } }, loadedPlugins[name].injects[0]._config?.call(loadedPlugins[name], tools)))
     }
+})()
+
 })
 )";
 
