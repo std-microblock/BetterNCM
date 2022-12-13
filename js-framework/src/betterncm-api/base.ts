412d36d32e05ebcc7e81e452033dbde3d375adc5
Steve-xmh <39523898+Steve-xmh@users.noreply.github.com>
2022-12-13 20:50:42 +0800

    Fixed

--- js-framework/src/betterncm-api/base.ts
+++ js-framework/src/betterncm-api/base.ts
@@ -5,12 +5,13 @@ window.BETTERNCM_FILES_PATH = BETTERNCM_FILES_PATH;
 export const ncmFetch = (
 	relPath: string,
 	option?: RequestInit & {
-		ignoreApiKey?: boolean	
+		ignoreApiKey?: boolean;
 	},
 ) => {
 	if (option) {
 		option.headers = option.headers ?? {};
-		if (!option.ignoreApiKey) option.headers["BETTERNCM_API_KEY"] = BETTERNCM_API_KEY;
+		if (!option.ignoreApiKey)
+			option.headers["BETTERNCM_API_KEY"] = BETTERNCM_API_KEY;
 	}
 	return fetch(BETTERNCM_API_PATH + relPath, option);
 };
---
+++ js-framework/src/plugin-manager/components/warning.tsx
@@ -0,0 +1,13 @@
+/**
+ * @fileoverview
+ * 首次启动会弹出的免责和警告窗口
+ * 
+ * 无良公众号和倒卖狗去死吧
+ */
+
+const StartupWarning: React.FC = () => {
+    return <div className="startup-warning">
+        <h2>欢迎使用 BetterNCM！</h2>
+        <p>BetterNCM 是一个</p>
+    </div>
+}
