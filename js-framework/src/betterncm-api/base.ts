3dc67faee48108dd8c5b5df4aa7f9e6ab7884f7f
Steve-xmh <39523898+Steve-xmh@users.noreply.github.com>
2022-12-13 20:50:07 +0800

    make ncmFetch api key optional

--- js-framework/src/betterncm-api/base.ts
+++ js-framework/src/betterncm-api/base.ts
@@ -4,16 +4,13 @@ window.BETTERNCM_API_PATH = BETTERNCM_API_PATH;
 window.BETTERNCM_FILES_PATH = BETTERNCM_FILES_PATH;
 export const ncmFetch = (
 	relPath: string,
-	option?: RequestInit,
-	withApiKey: boolean = true,
+	option?: RequestInit & {
+		ignoreApiKey?: boolean	
+	},
 ) => {
 	if (option) {
 		option.headers = option.headers ?? {};
-		if (withApiKey) option.headers["BETTERNCM_API_KEY"] = BETTERNCM_API_KEY;
-	} else {
-		option={
-			headers: withApiKey ? { BETTERNCM_API_KEY } : {},
-		};
+		if (!option.ignoreApiKey) option.headers["BETTERNCM_API_KEY"] = BETTERNCM_API_KEY;
 	}
 	return fetch(BETTERNCM_API_PATH + relPath, option);
 };
